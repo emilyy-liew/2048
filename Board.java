d263d0aac8edf77e0a175864990b6c2396ab2a56
Emily Liew <53126513+emilyy-liew@users.noreply.github.com>
2022-10-04 10:20:28 -0400

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,351 @@
+public class Board {
+    private int[][] numbers;
+    private String[] maxCol;
+    private static int score;
+    private static String isWon;
+    private boolean sameBoard;
+
+    Board() {
+        numbers = new int[4][4];
+        maxCol = new String[4];
+        score = 0;
+        setIsWon("");
+        setSameBoard(false);
+        for (int x = 0; x < 2; x++) {
+            addNewNumbers();
+        }
+    }
+
+    public void printBoard() {
+        for(int c = 0; c < 4; c++) {
+            int max = 0;
+            for (int r = 0; r < 4; r++) {
+                if(numbers[r][c] > max) {
+                    max = numbers[r][c];
+                }
+            }
+            maxCol[c] = "" + max;
+        }
+        String m = "" + calculateMax();
+        System.out.println("Score: " + score);
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 4; c++) {
+                if(numbers[r][c] == 0) {
+                    System.out.print("_");
+                } else {
+                    System.out.print(numbers[r][c]);
+                }
+                String temp = "" + numbers[r][c];
+                for(int x = 0; x < maxCol[c].length() - temp.length() + 1; x++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public void setSameBoard(boolean sameBoard) {
+        this.sameBoard = sameBoard;
+    }
+
+    private void addNewNumbers() {
+        int row, col;
+        if (isBoardFull()) {
+            // do nothing
+        } else {
+            do {
+                row = (int) (Math.random() * 4);
+                col = (int) (Math.random() * 4);
+            } while (!(numbers[row][col] == 0));
+
+            numbers[row][col] = (((int)(Math.random() * 10) % 2 == 0) ? 2 : 4);
+        }
+    }
+
+    private int calculateScore(int mergedSquare, int score) {
+        if(mergedSquare == 2) {
+            return 0;
+        }
+        return score + (2 * calculateScore((int) (mergedSquare * Math.pow(2, -1)), 0) + mergedSquare);
+    }
+
+    public String checkBoard() {
+        if (!sameBoard) {
+            if(calculateMax() == 2048) {
+                return "won";
+            }
+        }
+
+        if (isBoardFull() && !canMerge()) {
+            return "lost";
+        }
+        return "";
+    }
+
+    private boolean isBoardFull() {
+        for (int[] y: numbers)
+            for(int x: y)
+                if (x == 0)
+                    return false;
+        return true;
+    }
+
+    private boolean canMergeHorizontally() {
+        return canMoveLeft() || canMoveRight();
+    }
+
+    private boolean canMoveLeft() {
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (numbers[r][c] == 0 && numbers[r][c + 1] != 0) {
+                    return true;
+                }
+            }
+        }
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (numbers[r][c] == numbers[r][c + 1] && !(numbers[r][c] == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canMoveRight() {
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (numbers[r][c] != 0 && numbers[r][c + 1] == 0) {
+                    return true;
+                }
+            }
+        }
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (numbers[r][c] == numbers[r][c + 1] && !(numbers[r][c] == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canMergeVertically() {
+        return canMoveUp() || canMoveDown();
+    }
+
+    private boolean canMoveUp() {
+        for(int c = 0; c < 4; c++) {
+            for (int r = 0; r < 3; r++) {
+                if (numbers[r][c] == 0 && numbers[r + 1][c] != 0) {
+                    return true;
+                }
+            }
+        }
+        for(int c = 0; c < 4; c++) {
+            for (int r = 0; r < 3; r++) {
+                if (numbers[r][c] == numbers[r + 1][c] && !(numbers[r][c] == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canMoveDown() {
+        for(int c = 0; c < 4; c++) {
+            for (int r = 0; r < 3; r++) {
+                if (numbers[r+1][c] == 0 && numbers[r][c] != 0) {
+                    return true;
+                }
+            }
+        }
+        for(int c = 0; c < 4; c++) {
+            for (int r = 0; r < 3; r++) {
+                if (numbers[r][c] == numbers[r + 1][c] && !(numbers[r][c] == 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canMerge() {
+        return canMergeHorizontally() || canMergeVertically();
+    }
+
+    public int calculateMax() {
+        int max = 0;
+        for(int r = 0; r < 4; r++) {
+            for (int c = 0; c < 4; c++) {
+                if(numbers[r][c] > max)
+                    max = numbers[r][c];
+            }
+        }
+        return max;
+    }
+
+    public void moveW() {
+        if (canMoveUp()) {
+            for(int c = 0; c < 4; c++) {
+                int counter = 0;
+                for(int r = 0; r < 4; r++) {
+                    if(!(numbers[r][c] == 0)){
+                        numbers[counter][c] = numbers[r][c];
+                        if(!(r == counter))
+                            numbers[r][c] = 0;
+                        counter++;
+                    }
+                }
+            }
+            for(int c = 0; c < 4; c++) {
+                for (int r = 0; r < 3; r++) {
+                    if (numbers[r][c] == numbers[r + 1][c] && !(numbers[r][c] == 0)) {
+                        numbers[r][c] += numbers[r + 1][c];
+                        score = calculateScore(numbers[r][c], score);
+                        numbers[r + 1][c] = 0;
+                    }
+                }
+            }
+            for(int c = 0; c < 4; c++) {
+                int counter = 0;
+                for(int r = 0; r < 4; r++) {
+                    if(!(numbers[r][c] == 0)){
+                        numbers[counter][c] = numbers[r][c];
+                        if(!(r == counter))
+                            numbers[r][c] = 0;
+                        counter++;
+                    }
+                }
+            }
+            addNewNumbers();
+        }
+        printBoard();
+    }
+
+    public void moveA() {
+        if (canMoveLeft()) {
+            for(int r = 0; r < 4; r++) {
+                int counter = 0;
+                for(int c = 0; c < 4; c++) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[r][counter] = numbers[r][c];
+                        if(!(c == counter))
+                            numbers[r][c] = 0;
+                        counter++;
+                    }
+                }
+            }
+            for(int r = 0; r < 4; r++) {
+                for(int c = 0; c < 3; c++) {
+                    if(numbers[r][c] == numbers[r][c+1] && !(numbers[r][c] == 0)) {
+                        numbers[r][c] += numbers[r][c+1];
+                        score = calculateScore(numbers[r][c], score);
+                        numbers[r][c+1] = 0;
+                    }
+                }
+            }
+            for(int r = 0; r < 4; r++) {
+                int counter = 0;
+                for(int c = 0; c < 4; c++) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[r][counter] = numbers[r][c];
+                        if(!(c == counter))
+                            numbers[r][c] = 0;
+                        counter++;
+                    }
+                }
+            }
+            addNewNumbers();
+        }
+        printBoard();
+    }
+
+    public void moveS() {
+        if (canMoveDown()) {
+            for(int c = 0; c < 4; c++) {
+                int counter = 3;
+                for(int r = 3; r >= 0; r--) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[counter][c] = numbers[r][c];
+                        if(!(r == counter))
+                            numbers[r][c] = 0;
+                        counter--;
+                    }
+                }
+            }
+            for(int c = 0; c < 4; c++) {
+                for(int r = 3; r > 0; r--) {
+                    if(numbers[r][c] == numbers[r-1][c] && !(numbers[r][c] == 0)) {
+                        numbers[r][c] += numbers[r-1][c];
+                        score = calculateScore(numbers[r][c], score);
+                        numbers[r-1][c] = 0;
+                    }
+                }
+            }
+            for(int c = 0; c < 4; c++) {
+                int counter = 3;
+                for(int r = 3; r >= 0; r--) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[counter][c] = numbers[r][c];
+                        if(!(r == counter))
+                            numbers[r][c] = 0;
+                        counter--;
+                    }
+                }
+            }
+            addNewNumbers();
+
+
+
+        }
+        printBoard();
+    }
+
+    public void moveD() {
+        if (canMoveRight()) {
+            for(int r = 0; r < 4; r++) {
+                int counter = 3;
+                for(int c = 3; c >= 0; c--) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[r][counter] = numbers[r][c];
+                        if(!(c == counter))
+                            numbers[r][c] = 0;
+                        counter--;
+                    }
+                }
+            }
+            for(int r = 0; r < 4; r++) {
+                for(int c = 3; c > 0; c--) {
+                    if(numbers[r][c] == numbers[r][c-1] && !(numbers[r][c] == 0)) {
+                        numbers[r][c] += numbers[r][c-1];
+                        score = calculateScore(numbers[r][c], score);
+
+                        numbers[r][c-1] = 0;
+                    }
+                }
+            }
+            for(int r = 0; r < 4; r++) {
+                int counter = 3;
+                for(int c = 3; c >= 0; c--) {
+                    if(!(numbers[r][c] == 0)) {
+                        numbers[r][counter] = numbers[r][c];
+                        if(!(c == counter))
+                            numbers[r][c] = 0;
+                        counter--;
+                    }
+                }
+            }
+            addNewNumbers();
+        }
+        printBoard();
+    }
+
+    public String getIsWon() {
+        return isWon;
+    }
+
+    public void setIsWon(String isWon) {
+        this.isWon = isWon;
+    }
+}
--- two048.java
+++ two048.java
@@ -3,295 +3,74 @@ import java.util.Scanner;
 public class two048 {
 
     public static void main(String[] args) {
-            // TODO fix add new numbers when nothing changes
-            // TODO method everything
-            // TODO add user interface
-
-            int[][] numbers = new int[4][4];
-            String[] maxCol = new String[4];
-            int row = 0, col = 0, score = 0;
+        // TODO add user interface
+        String again;
+        do {
+            Board board = new Board();
             String move, game = "";
             Scanner in = new Scanner(System.in);
+            board.printBoard();
 
-            for (int x = 0; x < 2; x++) {
-                addNewNumbers(row, col, numbers);
-            }
-
-            printBoard(numbers, score, maxCol);
-
-            while (!(game.equals("won")|| game.equals("lost"))){
+            while (!(board.getIsWon().equals("won")|| board.getIsWon().equals("lost") || board.getIsWon().equals("again"))){
                 move = in.nextLine();
                 switch(move) {
                     case "w":
-                        for(int c = 0; c < 4; c++) {
-                            int counter = 0;
-                            for(int r = 0; r < 4; r++) {
-                                if(!(numbers[r][c] == 0)){
-                                    numbers[counter][c] = numbers[r][c];
-                                    if(!(r == counter))
-                                        numbers[r][c] = 0;
-                                    counter++;
-                                }
-                            }
-                        }
-                        for(int c = 0; c < 4; c++) {
-                            for (int r = 0; r < 3; r++) {
-                                if (numbers[r][c] == numbers[r + 1][c] && !(numbers[r][c] == 0)) {
-                                    numbers[r][c] += numbers[r + 1][c];
-                                    score = calculateScore(numbers[r][c], score);
-                                    numbers[r + 1][c] = 0;
-                                }
-                            }
-                        }
-                        for(int c = 0; c < 4; c++) {
-                            int counter = 0;
-                            for(int r = 0; r < 4; r++) {
-                                if(!(numbers[r][c] == 0)){
-                                    numbers[counter][c] = numbers[r][c];
-                                    if(!(r == counter))
-                                        numbers[r][c] = 0;
-                                    counter++;
-                                }
-                            }
-                        }
-                        addNewNumbers(row, col, numbers);
-                        printBoard(numbers, score, maxCol);
+                        board.moveW();
                         break;
                     case "a":
-                        for(int r = 0; r < 4; r++) {
-                            int counter = 0;
-                            for(int c = 0; c < 4; c++) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[r][counter] = numbers[r][c];
-                                    if(!(c == counter))
-                                        numbers[r][c] = 0;
-                                    counter++;
-                                }
-                            }
-                        }
-                        for(int r = 0; r < 4; r++) {
-                            for(int c = 0; c < 3; c++) {
-                                if(numbers[r][c] == numbers[r][c+1] && !(numbers[r][c] == 0)) {
-                                    numbers[r][c] += numbers[r][c+1];
-                                    score = calculateScore(numbers[r][c], score);
-                                    numbers[r][c+1] = 0;
-                                }
-                            }
-                        }
-                        for(int r = 0; r < 4; r++) {
-                            int counter = 0;
-                            for(int c = 0; c < 4; c++) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[r][counter] = numbers[r][c];
-                                    if(!(c == counter))
-                                        numbers[r][c] = 0;
-                                    counter++;
-                                }
-                            }
-                        }
-                        addNewNumbers(row, col, numbers);
-                        printBoard(numbers, score, maxCol);
+                        board.moveA();
                         break;
                     case "s":
-                        for(int c = 0; c < 4; c++) {
-                            int counter = 3;
-                            for(int r = 3; r >= 0; r--) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[counter][c] = numbers[r][c];
-                                    if(!(r == counter))
-                                        numbers[r][c] = 0;
-                                    counter--;
-                                }
-                            }
-                        }
-                        for(int c = 0; c < 4; c++) {
-                            for(int r = 3; r > 0; r--) {
-                                if(numbers[r][c] == numbers[r-1][c] && !(numbers[r][c] == 0)) {
-                                    numbers[r][c] += numbers[r-1][c];
-                                    score = calculateScore(numbers[r][c], score);
-                                    numbers[r-1][c] = 0;
-                                }
-                            }
-                        }
-                        for(int c = 0; c < 4; c++) {
-                            int counter = 3;
-                            for(int r = 3; r >= 0; r--) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[counter][c] = numbers[r][c];
-                                    if(!(r == counter))
-                                        numbers[r][c] = 0;
-                                    counter--;
-                                }
-                            }
-                        }
-                        addNewNumbers(row, col, numbers);
-                        printBoard(numbers, score, maxCol);
+                        board.moveS();
                         break;
                     case "d":
-                        for(int r = 0; r < 4; r++) {
-                            int counter = 3;
-                            for(int c = 3; c >= 0; c--) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[r][counter] = numbers[r][c];
-                                    if(!(c == counter))
-                                        numbers[r][c] = 0;
-                                    counter--;
-                                }
-                            }
-                        }
-                        for(int r = 0; r < 4; r++) {
-                            for(int c = 3; c > 0; c--) {
-                                if(numbers[r][c] == numbers[r][c-1] && !(numbers[r][c] == 0)) {
-                                    numbers[r][c] += numbers[r][c-1];
-                                    score = calculateScore(numbers[r][c], score);
-
-                                    numbers[r][c-1] = 0;
-                                }
-                            }
-                        }
-                        for(int r = 0; r < 4; r++) {
-                            int counter = 3;
-                            for(int c = 3; c >= 0; c--) {
-                                if(!(numbers[r][c] == 0)) {
-                                    numbers[r][counter] = numbers[r][c];
-                                    if(!(c == counter))
-                                        numbers[r][c] = 0;
-                                    counter--;
-                                }
-                            }
-                        }
-                        addNewNumbers(row, col, numbers);
-                        printBoard(numbers, score, maxCol);
+                        board.moveD();
                         break;
                     default:
-                        System.out.println("You suck, Anay!!!");
+                        System.out.println("Please enter a valid move key.");
                         break;
                 }
 
-                game = checkBoard(numbers);
-            }
-            if (game.equals("won")) {
-                System.out.println("Congrats you 2048 whiz!");
-            } else {
-                System.out.println("Better luck next time! Would you like to play again?");
-            }
-        }
-
-
-
-        public static void printBoard(int[][] arr, int score, String[] cols) {
-            for(int c = 0; c < 4; c++) {
-                int max = 0;
-                for (int r = 0; r < 4; r++) {
-                    if(arr[r][c] > max) {
-                        max = arr[r][c];
-                    }
-                }
-                cols[c] = "" + max;
-            }
-            String m = "" + calculateMax(arr);
-            System.out.println("Score: " + score);
-            for(int r = 0; r < 4; r++) {
-                for (int c = 0; c < 4; c++) {
-                    if(arr[r][c] == 0) {
-                        System.out.print("_");
+                board.setIsWon(board.checkBoard());
+                if (board.getIsWon().equals("won")) {
+                    System.out.println("Great job you 2048 whiz!");
+                    System.out.println("Would you like to continue your board? (Y/N)");
+                    board.setIsWon(in.nextLine());
+                    validateYorN(board, in);
+                    if(board.getIsWon().equals("Y") || board.getIsWon().equals("y")) {
+                        board.setSameBoard(true);
+                        board.printBoard();
                     } else {
-                        System.out.print(arr[r][c]);
+                        board.setIsWon("");
+                        System.out.println("Would like to play a new board? (Y/N)");
+                        board.setIsWon(in.nextLine());
+                        validateYorN(board, in);
+                        if(board.getIsWon().equals("Y") || board.getIsWon().equals("y")) {
+                            board.setIsWon("again");
+                        } else {
+                            board.setIsWon("won");
+                        }
                     }
-                    String temp = "" + arr[r][c];
-                    for(int x = 0; x < cols[c].length() - temp.length() + 1; x++) {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println("");
+                } else if (board.getIsWon().equals("lost") && board.calculateMax() != 2048) {
+                   System.out.println("Better luck next time! Would you like to play again? (Y/N)");
+                   board.setIsWon(in.nextLine());
+                   validateYorN(board, in);
+                   board.setIsWon((board.getIsWon().equals("Y") || board.getIsWon().equals("y")) ? "again" : "lost");
+               } else if (board.getIsWon().equals("lost")) {
+                   System.out.println("WOW great job! Would like to play again? (Y/N)");
+                   board.setIsWon(in.nextLine());
+                   validateYorN(board, in);
+                   board.setIsWon((game.equals("Y") || game.equals("y")) ? "again" : "lost");
+               }
             }
-        }
+            again = board.getIsWon();
+        } while (again.equals("again"));
+    }
 
-        public static void addNewNumbers(int row, int col, int[][] arr) {
-            if (isBoardFull(arr) || !canMerge(arr) && !(calculateMax(arr) <= 2)) {
-                // do nothing
-            } else {
-                do {
-                    row = (int) (Math.random() * 4);
-                    col = (int) (Math.random() * 4);
-                } while (!(arr[row][col] == 0));
-
-                arr[row][col] = (((Math.random() * 10) % 2 == 0) ? 2 : 4);
-            }
-        }
-
-        public static int calculateScore(int mergedSquare, int totalScore) {
-            if(mergedSquare == 2) {
-                return 0;
-            }
-            return totalScore + (2 * calculateScore((int) (mergedSquare * Math.pow(2, -1)), 0) + mergedSquare);
-        }
-
-        public static String checkBoard(int[][] arr) {
-            if(calculateMax(arr) == 2048) {
-                return "won";
-            } else if (isBoardFull(arr) && !canMerge(arr)) {
-                return "lost";
-            }
-            return "";
-        }
-
-        public static boolean isBoardFull(int[][] arr) {
-            for (int[] y: arr)
-                for(int x: y)
-                    if(x == 0)
-                        return false;
-            return true;
-        }
-
-        public static boolean canMergeHorizontally(int[][] arr) {
-            for(int r = 0; r < 4; r++) {
-                for (int c = 0; c < 3; c++) {
-                    if (arr[r][c] == arr[r][c + 1] && !(arr[r][c] == 0)) {
-                        return true;
-                    }
-                }
-            }
-            for(int r = 0; r < 4; r++) {
-                for (int c = 0; c < 3; c++) {
-                    if(!(arr[r][c] == arr[r][c + 1]) && arr[r][c] == 0 || arr[r][c+1] == 0) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public static boolean canMergeVertically(int[][] arr) {
-            for(int c = 0; c < 4; c++) {
-                for (int r = 0; r < 3; r++) {
-                    if (arr[r][c] == arr[r + 1][c] && !(arr[r][c] == 0)) {
-                        return true;
-                    }
-                }
-            }
-            for(int c = 0; c < 4; c++) {
-                for (int r = 0; r < 3; r++) {
-                    if(!(arr[r][c] == arr[r+1][c]) && arr[r][c] == 0 || arr[r+1][c] == 0) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public static boolean canMerge(int[][] arr) {
-            return canMergeHorizontally(arr) || canMergeVertically(arr);
-        }
-
-        public static int calculateMax(int[][] arr) {
-            int max = 0;
-            for(int r = 0; r < 4; r++) {
-                for (int c = 0; c < 4; c++) {
-                    if(arr[r][c] > max)
-                        max = arr[r][c];
-                }
-            }
-            return max;
+    public static void validateYorN(Board board, Scanner in) {
+        while(!(board.getIsWon().equals("Y") || board.getIsWon().equals("y") || board.getIsWon().equals("N") || board.getIsWon().equals("n"))) {
+            System.out.print("Please enter a valid selection: ");
+            board.setIsWon(in.nextLine());
         }
     }
+}
